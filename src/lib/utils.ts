ebad49af970cf4d71c84dc410b1814963d144f9a
Saurabh-112233 <comraded91@gmail.com>
2024-07-16 19:52:42 +0530

    update profile page changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -54,3 +54,4 @@ export const multiFormatDateString = (timestamp: string = ""): string => {
 export const checkIsLiked = (likeList: string[], userId: string) => {
   return likeList.includes(userId);
 };
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
